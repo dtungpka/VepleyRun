2af6d973c628f29be5bb7b533885fa38d8b4836d
DuongTung <duongdoantung2k3@gmail.com>
2023-02-25 03:58:45 +0700

    Update to Keras model

--- VepleyAI_train.py
+++ VepleyAI_train.py
@@ -4,7 +4,6 @@ from struct import unpack
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-import cv2
 import random
 import os
 import time
@@ -13,22 +12,24 @@ import sys
 import DNN
 import shutil
 from VepleyAI_acquire import Actions
+from tkinter import filedialog
+
 TRAIN_TEST_RATIO = 0.8
 #H: 480
 #W: 640
 H = 480
 W = 640
 dataFiles = "D:\\2021-2022\\Research\\Dataset\\VepleyAI"
-processed_folder = "./Processed/"
+processed_folder = "D:\\2021-2022\\Research\\VepleyRunAI\\Processed"
 Excluded = []
 parent_folder = True
 IGNORE_RIGHT_HAND = False
 FLIP_RIGHT_HAND = True
 LEARNING_RATE = 0.0001
-ITERATIONS = 40000
+ITERATIONS = 20
 #train the neural net with 256 
-LAYERS = [1, 256, 256, 256, len(Actions)]
-
+LAYERS = [512, 256, 256, len(Actions)]
+ACTIVATION = [ "relu", "relu", "relu", "sigmoid"]
 PARSE_LANDMARKS_JOINTS = [    
     [0, 1], [1, 2], [2, 3], [3, 4], # thumb
     [0, 5],[5, 6], [6, 7], [7, 8], # index finger
@@ -46,6 +47,7 @@ np.random.seed(1)
 class VepleyAiTrain():
     def __init__(self, filesPath = "Datasets",layers = LAYERS,train_ratio = 0.8 ,parentFolder = True,excluded = Excluded) -> None: 
         self.date = None
+        self.data = None
         #self.read_data(filesPath , parentFolder, excluded)
         pass
     def read_data(self,path: str,_parentFolder: bool = parent_folder, _excluded: list = Excluded):
@@ -69,8 +71,8 @@ class VepleyAiTrain():
         global processed_folder
         if not os.path.exists(processed_folder):
             os.mkdir(processed_folder)
-        pickle.dump(self.data,open(os.path.join(processed_folder,dataFiles)+".pickle","wb"))
-        print("Data processed,saved to "+processed_folder+dataFiles+".pickle")
+        pickle.dump(self.data,open(os.path.join(processed_folder,dataFiles)+".bin","wb"))
+        print("Data processed,saved to "+processed_folder+dataFiles+".bin")
     def unpack(self,path,parentFolder):
         if parentFolder:
             for VAID in os.listdir(path):
@@ -90,13 +92,13 @@ class VepleyAiTrain():
                     #read detail.json, sum the total
                     with open(os.path.join(path,folder,"detail.json"),"r") as f:
                         detail = json.load(f)
-                        NO_OF_SAMPLES += detail['total']
+                        NO_OF_SAMPLES += detail['total']*2
                         W = detail['width']
                         H = detail['height']
         else:
             with open(os.path.join(path,"detail.json"),"r") as f:
                 detail = json.load(f)
-                NO_OF_SAMPLES = detail['total']
+                NO_OF_SAMPLES = detail['total']*2
 
                 W = detail['width']
                 H = detail['height']
@@ -114,89 +116,91 @@ class VepleyAiTrain():
                     for hand in raw_datas[ID]:
                         if IGNORE_RIGHT_HAND and hand == '1':
                             continue
-                        self.data['ID'][int(ID)] = int(ID+hand)
-                        self.data['Y'][int(ID)] = j
+                        if hand == '0':
+                            self.data['ID'][int(ID)] = int(ID+hand)
+                            self.data['Y'][int(ID)] = j
+                        else:
+                            self.data['ID'][int(ID)*2] = int(ID+hand)
+                            self.data['Y'][int(ID)*2] = j
                         landmarks = {point[0]:np.array(point[1:]) for point in raw_datas[ID][hand]}
                         if FLIP_RIGHT_HAND and hand == '1':
                             for k in landmarks:
                                 landmarks[k][0] = W - landmarks[k][0]
                         for i,(_p1,_p2) in enumerate(PARSE_LANDMARKS_JOINTS):
                             if _p1 in landmarks and _p2 in landmarks:
-                                self.data['X'][i][int(ID)] = self.calculate_angle(landmarks[_p1],landmarks[_p2])
+                                tmp = self.calculate_angle(landmarks[_p1],landmarks[_p2])
+                                if hand == '0':
+                                    self.data['X'][i][int(ID)] = tmp
+                                self.data['X'][i][int(ID)*2] = tmp
                             else:
-                                self.data['X'][i][int(ID)] = 0
+                                if hand == '0':
+                                    self.data['X'][i][int(ID)] = 0
+                                self.data['X'][i][int(ID)*2] = 0
             print("\rProcessed "+action,f'{j+1} out of {len(Actions)} ',end='\n')     
     def calculate_angle(self,landmark1, landmark2):
         return np.math.atan2(np.linalg.det([landmark1, landmark2]), np.dot(landmark1, landmark2))
     def load(self):
         if self.data == None:
-            files = os.listdir(processed_folder)
-            if len(files) == 0:
-                print("No processed data found, please process data first")
-                return
-            else:
-                #read the lastest data
-                self.data = pickle.load(open(processed_folder+files[-1],"rb"))
-                print("Loaded data from "+processed_folder+files[-1])
+            #prompt to select file, end with .bin
+            global processed_folder
+            filename = filedialog.askopenfilename(initialdir = processed_folder,title = "Select file",filetypes = (("bin files","*.bin"),("all files","*.*")))
+            if filename:
+                self.data = pickle.load(open(filename,"rb"))
+                print("Data loaded from "+filename)
+            
         
         #spilt the data
         self._split_data()
     def _split_data(self):
         global TRAIN_TEST_RATIO,NO_OF_SAMPLES
-        train_size = int(NO_OF_SAMPLES*TRAIN_TEST_RATIO)
-        self.X = self.data['X'][:train_size,:].T
-        self.Y = self.data['Y'][:train_size].T
-        #from Y (800,) to (800,5)
-        self.Y = np.eye(len(Actions))[self.Y.astype(int)].T
 
-        self.X_test = self.data['X'][train_size:,:].T
-        self.Y_test = self.data['Y'][train_size:].T
+        pmt = np.random.permutation(NO_OF_SAMPLES)
+        self.data['X'] = self.data['X'][pmt,:]
+        self.data['Y'] = self.data['Y'][pmt]
+        
+        train_size = int(NO_OF_SAMPLES*TRAIN_TEST_RATIO)
+        self.X = self.data['X'][:train_size,:]
+        self.Y = self.data['Y'][:train_size]
+        #from Y (800,) to (800,5)
+        self.Y = np.eye(len(Actions))[self.Y.astype(int)]
+        
+        self.X_test = self.data['X'][train_size:,:]
+        self.Y_test = np.eye(len(Actions))[(self.data['Y'][train_size:]).astype(int)]
         #print all the shape
         print("X shape: ",self.X.shape)
         print("Y shape: ",self.Y.shape)
         print("X_test shape: ",self.X_test.shape)
         print("Y_test shape: ",self.Y_test.shape)
     def train(self):
-        self.num_feature = self.X.shape[0]
-        self.num_class = len(Actions)
-        self.parameters = DNN.initialize_parameters_deep([self.num_feature, self.num_feature,  self.num_class])
-        _parameters = self.parameters
-        history = []
-        cost = 100
-        for i in range(0, ITERATIONS):
-            AL, caches = DNN.L_model_forward(self.X, _parameters)
-            grads = DNN.L_model_backward(AL, self.Y, caches)
-            _cost = np.sum(DNN.compute_cost(AL, self.Y))
-            _parameters = DNN.update_parameters(_parameters, grads, learning_rate = LEARNING_RATE)
-            if (_cost < cost ):
-                self.parameters = _parameters
-                cost = _cost
-            history.append(np.sum(_cost))
-            if i % 100 == 0:
-                print ("\rCost after iteration %i: " %(i),np.sum(cost),end='')
-       
-        plt.plot(history)
-        plt.ylabel('cost')
-        plt.xlabel('iterations (per hundreds)')
-        plt.title("Learning rate =" + str(LEARNING_RATE))
-        plt.show()
-        self.predict_random()
+        global LAYERS, ITERATIONS, ACTIVATION
+        if self.X is None:
+            print("No data to train")
+            return
+        self.model = DNN.create_model(LAYERS,ACTIVATION)
+        self.parameters = DNN.train_model(self.model,self.X,self.Y,self.X_test,self.Y_test,ITERATIONS)
         self.save_parameters()
+        DNN.plot_model_history(self.parameters)
+        self.predict_random()
+        
     def predict_random(self):
         if self.X_test is None:
             print("No data to predict")
             return
         for i in range(0,10):
             index = random.randint(0,self.X_test.shape[1])
-            print("Predicted: ",Actions[DNN.predict(self.X_test[:,index],self.parameters)])
-            print("Actual: ",Actions[self.Y_test[:,index].argmax()])
+            X_pred = self.X_test[index,:]
+            Y_pred = DNN.predict(self.model,X_pred.reshape(1,20))
+            print("Predicted: ",Actions[np.argmax(Y_pred)])
+            Y_true = self.Y_test[index,:]
+            print("True: ",Actions[np.argmax(Y_true)])
+            
     def save_parameters(self):
         if input("Save parameters? (y/n)").lower() != "y":
             return
         global processed_folder
         if not os.path.exists(processed_folder):
             os.mkdir(processed_folder)
-        pickle.dump(self.parameters,open(processed_folder+dataFiles+"_parameters.pickle","wb"))
+        pickle.dump(self.parameters,open(os.path.join(processed_folder,dataFiles)+"_parameters.bin","wb"))
         print("Parameters saved to "+processed_folder+dataFiles+"_parameters.bin")
         
         
@@ -216,8 +220,8 @@ if __name__ == "__main__":
     else:
         print("No arguments given, please use --process or --train")
         input("Press enter for debug mode")
-        instance.read_data(dataFiles,not s, Excluded)
-        instance.save_processed()
+        #instance.read_data(dataFiles,not s, Excluded)
+        #instance.save_processed()
         instance.load()
         instance.train()
       
